3d603522e26d540b0b40d7792d22406e7d93c0bf
wf90163 <wf90163@alibaba-inc.com>
2017-04-10 18:14:38 +0800

    release 6.4.3

--- proj.android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/umeng/Documents/source/cocos2sx3.12/UmengGame/proj.android/bin/AndroidManifest.xml : \
-/Users/umeng/Documents/source/cocos2sx3.12/UmengGame/proj.android/AndroidManifest.xml \
